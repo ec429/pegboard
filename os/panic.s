4fd61519450d78aee871e28102fd6e2eb1835dda
Edward Cree <ec429@cantab.net>
2014-07-30 00:33:41 +0100

    os: Add a panic() function, prints CPUid, pid and 16 stack items

---
+++ os/panic.s
@@ -0,0 +1,41 @@
+.text
+
+.globl panic
+panic:
+	LD IX,kprint_lock
+	CALL spin_lock
+	LD HL,panic_msg_1
+	CALL kputs_unlocked
+	LD A,(IY+0)		; cpuid
+	CALL kprint_hex_unlocked
+	LD HL,panic_msg_2
+	CALL kputs_unlocked
+	LD A,(IY+1)		; pid
+	CALL kprint_hex_unlocked
+	LD HL,panic_msg_3
+	CALL kputs_unlocked
+	LD B,0x10		; number of stack entries to print
+_panic_stack_loop:
+	POP HL
+	LD A,H
+	CALL kprint_hex_unlocked
+	LD A,L
+	CALL kprint_hex_unlocked
+	LD A,0x0a
+	CALL kputc_unlocked
+	DJNZ _panic_stack_loop
+	LD HL,panic_msg_4
+	CALL kputs_unlocked
+	CALL spin_unlock
+	DI
+	HALT
+
+.data
+panic_msg_1: .asciz "KERNEL PANIC on CPU #"
+panic_msg_2: .asciz " (pid="
+panic_msg_3: .ascii "), halting."
+.byte 0x0a
+.ascii "Stack:"
+.byte 0x0a,0
+panic_msg_4: .ascii "cut here: ------8<------"
+.byte 0x0a,0
